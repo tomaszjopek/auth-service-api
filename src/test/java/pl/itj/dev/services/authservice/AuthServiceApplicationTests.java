ac102fb63b55f95aa40a65f27ed52746d92742a3
Tomasz <tom.jopek@gmail.com>
2020-03-21 16:25:08 +0100

    initial commit

---
+++ src/test/java/pl/itj/dev/services/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.itj.dev.services.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
